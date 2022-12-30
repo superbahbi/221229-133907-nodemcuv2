88ec1d5e6ef2dbfb9d5127441d19d8790eb6466c
Bobby Lee <superbahbi@gmail.com>
2022-12-29 22:49:52 -0800

    remove date and time serial monitor

--- src/arduino-tft-display.cpp
+++ src/arduino-tft-display.cpp
@@ -47,19 +47,6 @@ void loop()
 {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    // printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-
-    Serial.print(tm.tm_year + 1900);
-    Serial.print("-");
-    Serial.print(tm.tm_mon + 1);
-    Serial.print("-");
-    Serial.print(tm.tm_mday);
-    Serial.print(" ");
-    Serial.print(tm.tm_hour);
-    Serial.print(":");
-    Serial.print(tm.tm_min);
-    Serial.print(":");
-    Serial.println(tm.tm_sec);
 
     tft.fillScreen(ST77XX_WHITE);
     char fullyear[50];
